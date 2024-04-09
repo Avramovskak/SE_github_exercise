6d05cff39575e936e708a7f86838da12991bee3d
Student <student@uacs.edu.mk>
2024-04-09 15:14:40 +0200

    adding starting projects

---
+++ ex1.py
@@ -0,0 +1,8 @@
+import module
+import kiki.multipy
+from eee import dev
+
+print(module.addition(5, 5))
+print(module.substract(10 , 5))
+print(kiki.multipy.multiply(10, 5))
+print(dev.devision(10, 5))
